dab550cfb99c856275a5e76d2a8c150ac49c4a82
imeshthana <imeshthanapathi@gmail.com>
2025-02-23 00:10:58 +0530

    Initial commit

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Voting = await hre.ethers.getContractFactory("Voting");
+  const voting = await Voting.deploy(["Mark", "Mike", "Henry", "Rock"], 90);
+  await voting.waitForDeployment();
+  console.log("Contract Address", await voting.getAddress());
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
